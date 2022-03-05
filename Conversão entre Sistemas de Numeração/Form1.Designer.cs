f7ad639989fc34e41496dcec63995c8b821ae6a6
Carlom2003pt <carlom634@gmail.com>
2022-03-05 19:07:22 +0000

    Conversão entre Sistemas de Numeração

---
+++ Conversão entre Sistemas de Numeração/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Conversão_entre_Sistemas_de_Numeração
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxNDecimal = new System.Windows.Forms.TextBox();
+            this.textBoxNBinário = new System.Windows.Forms.TextBox();
+            this.textBoxNODecimal = new System.Windows.Forms.TextBox();
+            this.textBoxNOctal = new System.Windows.Forms.TextBox();
+            this.textBoxNHDecimal = new System.Windows.Forms.TextBox();
+            this.textBoxNHexadecimal = new System.Windows.Forms.TextBox();
+            this.buttonCDBinário = new System.Windows.Forms.Button();
+            this.buttonCBDecimal = new System.Windows.Forms.Button();
+            this.buttonCODecimal = new System.Windows.Forms.Button();
+            this.buttonCDOctal = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.buttonCHDecimal = new System.Windows.Forms.Button();
+            this.buttonCDHexadecimal = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textboxResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número decimal:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número binário:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Número decimal:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(28, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Número octal:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(28, 345);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Número decimal:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(28, 381);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(144, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Número hexadecimal:";
+            // 
+            // textBoxNDecimal
+            // 
+            this.textBoxNDecimal.Location = new System.Drawing.Point(139, 65);
+            this.textBoxNDecimal.Name = "textBoxNDecimal";
+            this.textBoxNDecimal.Size = new System.Drawing.Size(252, 20);
+            this.textBoxNDecimal.TabIndex = 6;
+            // 
+            // textBoxNBinário
+            // 
+            this.textBoxNBinário.Location = new System.Drawing.Point(139, 102);
+            this.textBoxNBinário.Name = "textBoxNBinário";
+            this.textBoxNBinário.Size = new System.Drawing.Size(252, 20);
+            this.textBoxNBinário.TabIndex = 7;
+            // 
+            // textBoxNODecimal
+            // 
+            this.textBoxNODecimal.Location = new System.Drawing.Point(139, 200);
+            this.textBoxNODecimal.Name = "textBoxNODecimal";
+            this.textBoxNODecimal.Size = new System.Drawing.Size(252, 20);
+            this.textBoxNODecimal.TabIndex = 8;
+            // 
+            // textBoxNOctal
+            // 
+            this.textBoxNOctal.Location = new System.Drawing.Point(139, 235);
+            this.textBoxNOctal.Name = "textBoxNOctal";
+            this.textBoxNOctal.Size = new System.Drawing.Size(252, 20);
+            this.textBoxNOctal.TabIndex = 9;
+            // 
+            // textBoxNHDecimal
+            // 
+            this.textBoxNHDecimal.Location = new System.Drawing.Point(148, 342);
+            this.textBoxNHDecimal.Name = "textBoxNHDecimal";
+            this.textBoxNHDecimal.Size = new System.Drawing.Size(252, 20);
+            this.textBoxNHDecimal.TabIndex = 10;
+            // 
+            // textBoxNHexadecimal
+            // 
+            this.textBoxNHexadecimal.Location = new System.Drawing.Point(169, 378);
+            this.textBoxNHexadecimal.Name = "textBoxNHexadecimal";
+            this.textBoxNHexadecimal.Size = new System.Drawing.Size(252, 20);
+            this.textBoxNHexadecimal.TabIndex = 11;
+            // 
+            // buttonCDBinário
+            // 
+            this.buttonCDBinário.Location = new System.Drawing.Point(397, 63);
+            this.buttonCDBinário.Name = "buttonCDBinário";
+            this.buttonCDBinário.Size = new System.Drawing.Size(338, 31);
+            this.buttonCDBinário.TabIndex = 12;
+            this.buttonCDBinário.Text = "Converter para Binário";
+            this.buttonCDBinário.UseVisualStyleBackColor = true;
+            this.buttonCDBinário.Click += new System.EventHandler(this.buttonCDBinário_Click_1);
+            // 
+            // buttonCBDecimal
+            // 
+            this.buttonCBDecimal.Location = new System.Drawing.Point(397, 100);
+            this.buttonCBDecimal.Name = "buttonCBDecimal";
+            this.buttonCBDecimal.Size = new System.Drawing.Size(338, 31);
+            this.buttonCBDecimal.TabIndex = 13;
+            this.buttonCBDecimal.Text = "Converter para Decimal";
+            this.buttonCBDecimal.UseVisualStyleBackColor = true;
+            this.buttonCBDecimal.Click += new System.EventHandler(this.buttonCBDecimal_Click_1);
+            // 
+            // buttonCODecimal
+            // 
+            this.buttonCODecimal.Location = new System.Drawing.Point(397, 228);
+            this.buttonCODecimal.Name = "buttonCODecimal";
+            this.buttonCODecimal.Size = new System.Drawing.Size(338, 27);
+            this.buttonCODecimal.TabIndex = 15;
+            this.buttonCODecimal.Text = "Converter para Decimal";
+            this.buttonCODecimal.UseVisualStyleBackColor = true;
+            this.buttonCODecimal.Click += new System.EventHandler(this.buttonCODecimal_Click_1);
+            // 
+            // buttonCDOctal
+            // 
+            this.buttonCDOctal.Location = new System.Drawing.Point(397, 190);
+            this.buttonCDOctal.Name = "buttonCDOctal";
+            this.buttonCDOctal.Size = new System.Drawing.Size(338, 30);
+            this.buttonCDOctal.TabIndex = 16;
+            this.buttonCDOctal.Text = "Converter para Octal";
+            this.buttonCDOctal.UseVisualStyleBackColor = true;
+            this.buttonCDOctal.Click += new System.EventHandler(this.buttonCDOctal_Click_1);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(26, 25);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(315, 26);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Sistema de Numeração Binario";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(26, 157);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(298, 26);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Sistema de Numeração Octal";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(26, 288);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(374, 26);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Sistema de Numeração Hexadecimal";
+            // 
+            // buttonCHDecimal
+            // 
+            this.buttonCHDecimal.Location = new System.Drawing.Point(433, 381);
+            this.buttonCHDecimal.Name = "buttonCHDecimal";
+            this.buttonCHDecimal.Size = new System.Drawing.Size(338, 31);
+            this.buttonCHDecimal.TabIndex = 22;
+            this.buttonCHDecimal.Text = "Converter para Decimal";
+            this.buttonCHDecimal.UseVisualStyleBackColor = true;
+            this.buttonCHDecimal.Click += new System.EventHandler(this.buttonCHDecimal_Click);
+            // 
+            // buttonCDHexadecimal
+            // 
+            this.buttonCDHexadecimal.Location = new System.Drawing.Point(415, 342);
+            this.buttonCDHexadecimal.Name = "buttonCDHexadecimal";
+            this.buttonCDHexadecimal.Size = new System.Drawing.Size(338, 31);
+            this.buttonCDHexadecimal.TabIndex = 21;
+            this.buttonCDHexadecimal.Text = "Converter para Hexadecimal";
+            this.buttonCDHexadecimal.UseVisualStyleBackColor = true;
+            this.buttonCDHexadecimal.Click += new System.EventHandler(this.buttonCDHexadecimal_Click_1);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(245, 426);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(86, 20);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Resultado:";
+            // 
+            // textboxResultado
+            // 
+            this.textboxResultado.Location = new System.Drawing.Point(249, 450);
+            this.textboxResultado.Name = "textboxResultado";
+            this.textboxResultado.Size = new System.Drawing.Size(229, 20);
+            this.textboxResultado.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 508);
+            this.Controls.Add(this.textboxResultado);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.buttonCHDecimal);
+            this.Controls.Add(this.buttonCDHexadecimal);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.buttonCDOctal);
+            this.Controls.Add(this.buttonCODecimal);
+            this.Controls.Add(this.buttonCBDecimal);
+            this.Controls.Add(this.buttonCDBinário);
+            this.Controls.Add(this.textBoxNHexadecimal);
+            this.Controls.Add(this.textBoxNHDecimal);
+            this.Controls.Add(this.textBoxNOctal);
+            this.Controls.Add(this.textBoxNODecimal);
+            this.Controls.Add(this.textBoxNBinário);
+            this.Controls.Add(this.textBoxNDecimal);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxNDecimal;
+        private System.Windows.Forms.TextBox textBoxNBinário;
+        private System.Windows.Forms.TextBox textBoxNODecimal;
+        private System.Windows.Forms.TextBox textBoxNOctal;
+        private System.Windows.Forms.TextBox textBoxNHDecimal;
+        private System.Windows.Forms.TextBox textBoxNHexadecimal;
+        private System.Windows.Forms.Button buttonCDBinário;
+        private System.Windows.Forms.Button buttonCBDecimal;
+        private System.Windows.Forms.Button buttonCODecimal;
+        private System.Windows.Forms.Button buttonCDOctal;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button buttonCHDecimal;
+        private System.Windows.Forms.Button buttonCDHexadecimal;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textboxResultado;
+    }
+}
+
---
+++ Conversão entre Sistemas de Numeração/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversão_entre_Sistemas_de_Numeração
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private string convHex(int r)
+        {
+            string v = "";
+            if (r == 10)
+                v = "A";
+            else if (r == 11)
+                v = "B";
+            else if (r == 12)
+                v = "C";
+            else if (r == 13)
+                v = "D";
+            else if (r == 14)
+                v = "E";
+            else if (r == 15)
+                v = "F";
+            else
+                v = r.ToString();
+            return v;
+        }
+
+        private int conValor(string s)
+        {
+            int j = 0;
+            switch (s)
+            {
+                case "A":
+                    j = 10;
+                    break;
+                case "B":
+                    j = 11;
+                    break;
+                case "C":
+                    j = 12;
+                    break;
+                case "D":
+                    j = 13;
+                    break;
+                case "E":
+                    j = 14;
+                    break;
+                case "F":
+                    j = 15;
+                    break;
+                default:
+                    j = int.Parse(s);
+
+                    break;
+
+            }
+            return j;
+        }
+
+        private void buttonCDBinário_Click_1(object sender, EventArgs e)
+        {
+            string s = "";
+            if (textBoxNDecimal.Text != "")
+            {
+                int n = int.Parse(textBoxNDecimal.Text);
+                while (n != 1)
+                {
+                    s = (n % 2).ToString() + s;
+
+                    n = n / 2;
+
+
+                }
+                s = "1" + s;
+                textboxResultado.Text = s;
+            }
+        }
+
+        private void buttonCBDecimal_Click_1(object sender, EventArgs e)
+        {
+            double res = 0;
+            string s = textBoxNBinário.Text;
+            if (s != "")
+            {
+                int t = s.Length;
+                for (int i = 0; i < t; i++)
+                {
+                    int ex = t - i - 1;
+                    res = (int.Parse(s[i].ToString()) * Math.Pow(2, ex)) + res;
+                }
+            }
+            textboxResultado.Text = res.ToString();
+        }
+
+        private void buttonCDOctal_Click_1(object sender, EventArgs e)
+        {
+            string s = "";
+            if (textBoxNODecimal.Text != "")
+            {
+                int n = int.Parse(textBoxNODecimal.Text);
+                while ((n != 1) && (n != 0))
+                {
+                    s = (n % 8).ToString() + s;
+
+                    n = n / 8;
+
+                }
+                if (n != 0)
+                    s = n.ToString() + s;
+                textboxResultado.Text = s;
+            }
+        }
+
+        private void buttonCODecimal_Click_1(object sender, EventArgs e)
+        {
+            double res = 0;
+            string s = textBoxNOctal.Text;
+            if (s != "")
+            {
+                int t = s.Length;
+                for (int i = 0; i < t; i++)
+                {
+                    int ex = t - i - 1;
+                    res = (int.Parse(s[i].ToString()) * Math.Pow(8, ex)) + res;
+                }
+            }
+            textboxResultado.Text = res.ToString();
+        }
+
+        private void buttonCDHexadecimal_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                string s = "";
+                if (textBoxNHDecimal.Text != "")
+                {
+                    int n = int.Parse(textBoxNHDecimal.Text);
+                    while (n != 0)
+                    {
+                        int resto = n % 16;
+                        string st = convHex(resto);
+                        s = st + s;
+                        n = n / 16;
+
+                    }
+                    if (n != 0)
+                        s = n.ToString() + s;
+                }
+                textboxResultado.Text = s;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("erro: " + ex.Message);
+            }
+        }
+
+        private void buttonCHDecimal_Click(object sender, EventArgs e)
+        {
+            double res = 0;
+            string s = textBoxNHexadecimal.Text;
+            int t = s.Length;
+            for (int i = 0; i < t; i++)
+            {
+                int ex = t - i - 1;
+                int val = conValor(s[i].ToString());
+                res = (val * Math.Pow(16, ex)) + res;
+            }
+            textboxResultado.Text = res.ToString();
+        }
+    }
+}
+
+
+
+
+
